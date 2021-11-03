dc1ac74166125353db8f7ee8dc390fcf580619a6
Thiago <thiago.kienbaum@hotmail.com>
2021-11-03 06:00:52 -0300

    add arquivos de conf

---
+++ grails-app/conf/UrlMappings.groovy
@@ -0,0 +1,20 @@
+class UrlMappings {
+
+	static mappings = {
+        "/api/account/$id?" {
+            controller = "account"
+            action = [GET:"show", POST:"save", PUT:"update", DELETE:"delete"]
+            format = "json"
+        }
+
+        "/api/transaction/$id?" {
+            controller = "transaction"
+            action = [GET:"show", POST:"save", PUT:"update", DELETE:"delete"]
+            format = "json"
+        }
+
+        "/"(controller: 'application', action:'index')
+        "500"(view: '/error')
+        "404"(view: '/notFound')
+	}
+}
